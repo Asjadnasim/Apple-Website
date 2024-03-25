3d7d8941d0c6ad58ba151eb974f330d2bac6fc0a
Asjad Nasim <asjadnasim@gmail.com>
2024-03-25 16:38:45 +0530

    Threejs ModalView

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,13 +1,14 @@
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import ModelView from './ModelView';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { yellowImg } from '../utils';
 
 import * as THREE from 'three';
 import { Canvas } from '@react-three/fiber';
 import { View } from '@react-three/drei';
 import { models, sizes } from '../constants';
+// import { animateWithGsapTimeline } from '../utils/animations';
 
 const Model = () => {
 	const [size, setSize] = useState('small');
@@ -17,7 +18,7 @@ const Model = () => {
 		img: yellowImg,
 	});
 
-	// camera contral for modal view
+	// camera control for the model view
 	const cameraControlSmall = useRef();
 	const cameraControlLarge = useRef();
 
@@ -28,6 +29,24 @@ const Model = () => {
 	// rotation
 	const [smallRotation, setSmallRotation] = useState(0);
 	const [largeRotation, setLargeRotation] = useState(0);
+
+	// const tl = gsap.timeline();
+
+	// useEffect(() => {
+	// 	if (size === 'large') {
+	// 		animateWithGsapTimeline(tl, small, smallRotation, '#view1', '#view2', {
+	// 			transform: 'translateX(-100%)',
+	// 			duration: 2,
+	// 		});
+	// 	}
+
+	// 	if (size === 'small') {
+	// 		animateWithGsapTimeline(tl, large, largeRotation, '#view2', '#view1', {
+	// 			transform: 'translateX(0)',
+	// 			duration: 2,
+	// 		});
+	// 	}
+	// }, [size]);
 
 	useGSAP(() => {
 		gsap.to('#heading', { y: 0, opacity: 1 });
@@ -61,6 +80,7 @@ const Model = () => {
 							item={model}
 							size={size}
 						/>
+
 						<Canvas
 							className='w-full h-full'
 							style={{
